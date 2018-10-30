066f1e3203a0c51c40cd43ced7fdad8262db7bd2
Dalton Saffe <daltonsaffe@gmail.com>
2018-10-30 19:39:55 -0400

    Delete unused reqs in app.js

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Navbar, CreateProduct, ProductList} from './components'
+import {Navbar} from './components'
 import Routes from './routes'
 
 const App = () => {
