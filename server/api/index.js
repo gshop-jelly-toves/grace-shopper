681f18aada8ae55cb572a30640ab5a42e9345526
benjaminpwagner@gmail.com <benjaminpwagner@gmail.com>
2018-11-05 16:02:02 -0500

    fixed the merge conflict

--- server/api/index.js
+++ server/api/index.js
@@ -4,12 +4,8 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/jellies', require('./jellies'))
 router.use('/cart', require('./cart'))
-<<<<<<< HEAD
 router.use('/stripe', require('./stripe'))
 
-=======
-router.use('/orders', require('./orders'))
->>>>>>> master
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
