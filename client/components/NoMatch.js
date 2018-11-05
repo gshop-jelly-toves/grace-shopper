5b6bfae2b7b4cb8e99ce8e9d53aa43a13fc72948
Christian Mejia <chrismejia10@gmail.com>
2018-11-05 10:20:04 -0500

    Comment out 404 image for fix prep

--- client/components/NoMatch.js
+++ client/components/NoMatch.js
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom'
 
 const NoMatch = () => {
   return (
-    <div className="d-flex flex-row align-items-center" id="error-cat">
+    <div className="d-flex flex-row align-items-center">
+      {/* id="error-cat"  isn't working*/}
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-12 text-center">
-            <img src="./ErrorHoneyCat.jpg" alt="ErrorHoneyCat" />
             {/* <span className="display-1 d-block">404</span> */}
             {/* <div className="mb-4 lead">
               The page you are looking for was not found.
