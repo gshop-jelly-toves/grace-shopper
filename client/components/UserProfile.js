ebbcb00df139fdbf12fd7d96852748626991f0b9
Christian Mejia <chrismejia10@gmail.com>
2018-11-06 20:49:56 -0500

    Debug commit; SingleJelly not rendering

--- client/components/UserProfile.js
+++ client/components/UserProfile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchUserProfile} from '../store/'
+import {Link} from 'react-router-dom'
 
 class UserProfile extends Component {
   componentDidMount() {
@@ -9,8 +10,9 @@ class UserProfile extends Component {
   }
 
   render() {
-    console.log(this.props.userProfile)
+    // console.log(this.props.userProfile)
     const {name, avatar, reviews} = this.props.userProfile
+    console.log('reviews', reviews)
     return (
       <div className="container">
         <div className="row p-3">
@@ -21,7 +23,46 @@ class UserProfile extends Component {
             <h1>Hello {name}</h1>
           </div>
         </div>
-        {/* {reviews.map(oneReview => (
+        <div className="row p-3">
+          <div className="col">
+            <h2>Reviews</h2>
+          </div>
+        </div>
+        {reviews ? (
+          reviews.map(userReview => (
+            <div key={userReview.id}>
+              <div className="row p-3">
+                <div className="col-3 col-md-3">
+                  {/* <Link to="/jellies/12"> */}
+                  <div className="card">
+                    <img className="card-img-top" src="" alt="Card image cap" />
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                    </div>
+                  </div>
+                  {/* </Link> */}
+                </div>
+                <div className="col-9 col-md-9">
+                  <h3>{userReview.title}</h3>
+
+                  {/* {for (let i = 0; i < user.Re)} */}
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No reviews here!</p>
+        )}
+      </div>
+    )
+  }
+}
+
+/* {reviews.map(oneReview => (
           <div key={oneReview.id}>
             <div className="row px-3">
               <div className="col">
@@ -43,11 +84,7 @@ class UserProfile extends Component {
               </div>
             </div>
           </div>
-        ))} */}
-      </div>
-    )
-  }
-}
+        ))} */
 
 const mapState = ({user: {userProfile}}) => ({
   userProfile
